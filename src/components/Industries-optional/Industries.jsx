af267c5f503890f25089b8ed4970b25614523076
hudir <hudirybw@gmail.com>
2022-06-10 22:04:18 +0200

    add optional to insight and industries

---
+++ src/components/Industries-optional/Industries.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Industries() {
+  return (
+    <div>Industries</div>
+  )
+}
+
+
+// if we found nice content we can keep this page, otherwise we can delete this page at the end
---
+++ src/components/Insights-optional/Insights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Insights() {
+  return (
+    <div>Insights</div>
+  )
+}
+
+// if we found nice content we can keep this page, otherwise we can delete this page at the end
